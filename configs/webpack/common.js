50ecb4dc2ffeaf65727e18fb70c2167c863ae96b
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-04-24 00:05:59 +0100

    Remove babel

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -11,11 +11,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: ['babel-loader', 'source-map-loader'],
-        exclude: /node_modules/,
-      },
-      {
         test: /\.tsx?$/,
         use: ['babel-loader', 'awesome-typescript-loader'],
       },
