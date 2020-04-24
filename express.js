2588a04cd56d700666fcae173c54133f799e2ec8
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-04-24 01:18:02 +0100

    Remove CORS

--- express.js
+++ express.js
@@ -7,31 +7,17 @@ const sourceDir = "dist";
 
 app.use(express.static(sourceDir));
 
-app.listen(portNumber, () => {
-  console.log(`Express web server started: http://localhost:${portNumber}`);
-  console.log(`Serving content from /${sourceDir}/`);
-});
-
-const whitelist = [
-  "http://localhost:3000",
-  "http://localhost:8080",
-  "https://react-rail.herokuapp.com/",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
 const options = {
   target: "http://api.irishrail.ie/realtime/realtime.asmx/",
   changeOrigin: true,
   ws: true,
 };
 
-app.use("/", cors(corsOptions), createProxyMiddleware(options));
+app.use("/", createProxyMiddleware(options));
+
+
+app.listen(portNumber, () => {
+  console.log(`Express web server started: http://localhost:${portNumber}`);
+  console.log(`Serving content from /${sourceDir}/`);
+});
+
