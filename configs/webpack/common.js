e24d4a70e6633c865cf313610dda76243b60f321
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-04-24 02:25:40 +0100

    Add favicon

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -1,47 +1,47 @@
 // shared config (dev and prod)
-const {resolve} = require('path');
-const {CheckerPlugin} = require('awesome-typescript-loader');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require("path");
+const { CheckerPlugin } = require("awesome-typescript-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
-  context: resolve(__dirname, '../../src'),
+  context: resolve(__dirname, "../../src"),
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: ['babel-loader', 'awesome-typescript-loader'],
+        use: ["babel-loader", "awesome-typescript-loader"],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }],
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+        ],
       },
       {
         test: /\.(scss|sass)$/,
         loaders: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'sass-loader',
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "sass-loader",
         ],
       },
       {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
-        ],
+        test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        loader: "file-loader?name=[name].[ext]", // <-- retain original file name
       },
     ],
   },
   plugins: [
     new CheckerPlugin(),
-    new HtmlWebpackPlugin({template: 'index.html.ejs',}),
+    new HtmlWebpackPlugin({ template: "index.html.ejs" }),
   ],
   externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM',
+    react: "React",
+    "react-dom": "ReactDOM",
   },
   performance: {
     hints: false,
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
+require("./favicon.ico");
 import App from "./components/App";
 
 const rootEl = document.getElementById("root");
