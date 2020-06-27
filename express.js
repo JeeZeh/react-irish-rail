32b90fde5573b147c0383f52c45b2271653ed59d
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2020-06-27 02:15:51 +0100

    try handle redirects to https, add theme, apple-touch

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const compression = require("compression");
+const sslRedirect = require("heroku-ssl-redirect");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const portNumber = 3000;
@@ -21,18 +22,27 @@ const corsOptions = {
   },
 };
 
-app.use(compression());
-app.use(express.static(sourceDir));
-
-app.listen(process.env.PORT ? process.env.PORT : portNumber, () => {
-  console.log(`Express web server started: http://localhost:${portNumber}`);
-  console.log(`Serving content from /${sourceDir}/`);
-});
-
-const options = {
+const proxyMiddlewareOptions = {
   target: "http://api.irishrail.ie/realtime/realtime.asmx/",
   changeOrigin: true,
   ws: true,
 };
 
-app.use("/", cors(corsOptions), createProxyMiddleware(options));
+app.use(compression());
+app.use(express.static(sourceDir));
+app.use(sslRedirect());
+
+app.use("/", cors(corsOptions), createProxyMiddleware(proxyMiddlewareOptions));
+
+app.get("*", (req, res, next) => {
+  if (req.headers["x-forwarded-proto"] != "https") {
+    res.redirect("https://" + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+app.listen(process.env.PORT ? process.env.PORT : portNumber, () => {
+  console.log(`Express web server started: http://localhost:${portNumber}`);
+  console.log(`Serving content from /${sourceDir}/`);
+});
